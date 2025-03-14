66716895a517e4ad7c18eae247b9b0a69762068b
Leon Chou <iamyiii0328@gmail.com>
2025-03-14 16:50:56 +0800

    feat: update header, css file

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,16 @@
 import PropTypes from 'prop-types'
 
-import classes from '@styles/layout/Header.module.scss'
-
 export default function Header({ appName }) {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>
-        <h1>{appName}</h1>
+    <header className='sticky top-0 px-[var(--header-padding-x)] flex justify-between items-center h-[var(--header-height)] z-50'>
+      <div>
+        <h1 className='text-logo tracking-logo'>{appName}</h1>
       </div>
 
-      <div className={classes.hamburger}>
-        <div className={classes.hamburgerContainer}>
-          <span></span>
-          <span></span>
+      <div className='w-[var(--hamburger-width)] h-[var(--hamburger-height)] rounded-full cursor-pointer flex items-center justify-center transition-all hover:bg-gray-02'>
+        <div className='p-2 w-8 h-8 flex flex-col justify-around items-center'>
+          <span className='block w-full h-[2px] rounded-sm bg-primary-02'></span>
+          <span className='block w-full h-[2px] rounded-sm bg-primary-02'></span>
         </div>
       </div>
     </header>
