42547509250ecf7d7f65975f9f4d18f80ab53a8e
Leon Chou <iamyiii0328@gmail.com>
2025-03-14 16:29:00 +0800

    feat: update project content page

--- src/components/pages/MandalaProjectPage.jsx
+++ src/components/pages/MandalaProjectPage.jsx
@@ -1,21 +1,63 @@
 import PropTypes from 'prop-types'
 
 import ProjectInfo from '@/components/ProjectInfo'
-import ProjectAbout from '@/components/ProjectAbout'
-import ProjectProcess from '@/components/ProjectProcess'
 import MandalaProcessChart from '@/components/mandala/MandalaProcessChart'
-import ProjectSitemap from '@/components/ProjectSitemap'
 import ProjectDemoVideo from '@/components/ProjectDemoVideo'
 import ProjectPureImagesDemo from '@/components/ProjectPureImagesDemo'
+
+import BaseSectionGrid from '@/components/templates/BaseSectionGrid'
+import ContentHeader from '@/components/molecules/ContentHeader'
+import SitemapIndicator from '@/components/SitemapIndicator'
+import BlurLazyImage from '@/components/ui/image/BlurLazyImage'
 
 const MandalaProjectPage = props => (
   <>
     <ProjectInfo {...props.info} />
-    <ProjectAbout {...props.about} />
-    <ProjectProcess {...props.process}>
-      <MandalaProcessChart {...props.process} />
-    </ProjectProcess>
-    <ProjectSitemap {...props.sitemap} />
+
+    {/* 關於專案 / About Project */}
+    <BaseSectionGrid
+      topContent={
+        <ContentHeader
+          title={props.about.title}
+          descriptions={props.about.descriptions}
+        />
+      }
+    />
+    {/* 專案流程 / Project Process */}
+    <BaseSectionGrid
+      topContent={
+        <ContentHeader
+          title={props.process.title}
+          descriptions={props.process.descriptions}
+        />
+      }
+      mainContent={<MandalaProcessChart {...props.process} />}
+    />
+    {/* 網站架構 / Sitemap */}
+    <BaseSectionGrid
+      topContent={
+        <ContentHeader
+          title={props.sitemap.title}
+          descriptions={props.sitemap.descriptions}
+        >
+          <SitemapIndicator indicators={props.sitemap.indicators} />
+        </ContentHeader>
+      }
+      mainContent={
+        <div>
+          <div className='w-full lg:w-[40.5%] mx-auto'>
+            {props.sitemap.images.map(img => (
+              <BlurLazyImage
+                key={img.id}
+                {...img}
+              />
+            ))}
+          </div>
+        </div>
+      }
+      hasDivider={false}
+    />
+
     <ProjectDemoVideo {...props.demoVideo} />
     <ProjectPureImagesDemo {...props.demonstrations} />
   </>
