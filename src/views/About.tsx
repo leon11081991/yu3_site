7504af2beddbc9d7533d7b750e0f3cfff5f78f42
Leon Chou <iamyiii0328@gmail.com>
2025-03-21 23:21:21 +0800

    fix(css): fix incorrect image path and text style color
    fix #26

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
           </div>
           <div className='max-w-[300px] w-full ml-auto mr-0 md:ml-[calc(30%+24px)] md:mr-auto -mt-[55px]'>
             <BlurLazyImage
-              src='/images/about/hero-image.avif'
+              src='/images/hero-image.avif'
               alt='Hero Image'
               width={300}
               height={169}
@@ -25,14 +25,14 @@ const About: React.FC = () => {
             <h2 className='flex-[30%] text-h2 font-h2'>關於我 / About</h2>
 
             <div className='flex-[70%] flex flex-col gap-10'>
-              <p className='text-p1 font-p1'>
+              <p className='text-p1 font-p1 text-gray-04'>
                 哈囉，我是 Lillian（林郁珊），一名從平面設計跨足 UI/UX 的設計師。自 2020 年首次參與
                 0 到 1 的產品開發，便對 UI/UX
                 設計產生濃厚興趣，也因此決定投入這個領域。這些年來，我累積了 4 年設計實務經驗，曾在
                 Agency 和 In-house
                 團隊工作，經歷不同產業與專案，讓我對設計的本質與價值有了更深刻的體悟。
               </p>
-              <p className='text-p1 font-p1'>
+              <p className='text-p1 font-p1 text-gray-04'>
                 我始終相信，
                 <span className='font-normal'>唯有持續學習、勇於跳脫舒適圈，才能不斷成長</span>
                 ，讓自己變得更加精實。在 AI
