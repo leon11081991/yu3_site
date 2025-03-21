568c38c2bf6e7147db2172defa51fc3781eb02cb
Leon Chou <iamyiii0328@gmail.com>
2025-03-21 23:17:50 +0800

    feat(pingo): update structure to shared layout component
    change layout to shared layout component, fix border buttom color
    
    fix #11

---
+++ src/features/projects/pingo/ResearchBarChartList.tsx
@@ -0,0 +1,39 @@
+import BarChartItem from '@/components/BarChartItem'
+
+interface ResearchBarChartListProps {
+  sources: {
+    title: string
+    source: string
+    chartSources: any[]
+  }[]
+}
+
+const ResearchBarChartList = ({ sources }: ResearchBarChartListProps) => {
+  return (
+    <div className='flex gap-14'>
+      {sources.map(({ title, source, chartSources }) => (
+        <ul
+          key={title}
+          className='flex-1 flex flex-col gap-10'
+        >
+          <li>
+            <div className=' flex flex-col gap-2'>
+              <p className='text-h3'>{title}</p>
+              <span className='text-p2 text-gray-04'>資料來源：{source}</span>
+            </div>
+            <div className='flex flex-col gap-6'>
+              {chartSources.map(item => (
+                <BarChartItem
+                  key={item.id}
+                  {...item}
+                />
+              ))}
+            </div>
+          </li>
+        </ul>
+      ))}
+    </div>
+  )
+}
+
+export default ResearchBarChartList
