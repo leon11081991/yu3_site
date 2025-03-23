5f19d6974011d42785326f66d52808803b175d29
Leon Chou <iamyiii0328@gmail.com>
2025-03-24 01:57:18 +0800

    fix(svg): fix svgicon doesnt show up
    addresses #10 #14 #16

--- src/hooks/useDynamicSvgImport.js
+++ src/hooks/useDynamicSvgImport.js
@@ -1,9 +1,10 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useReducer } from 'react'
 
 export function useDynamicSvgImport(iconName) {
   const importedIconRef = useRef()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
+  const [, forceUpdate] = useReducer(x => x + 1, 0)
 
   useEffect(() => {
     setLoading(true)
@@ -11,6 +12,7 @@ export function useDynamicSvgImport(iconName) {
     const importSvgIcon = async () => {
       try {
         importedIconRef.current = (await import(`@/assets/icons/${iconName}.svg`)).ReactComponent
+        forceUpdate()
       } catch (error) {
         setError(error)
         console.error(error)
